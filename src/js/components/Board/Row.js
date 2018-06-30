a92643538c0a81188e17c39522579b7565af6097
Kevin Hurlbut <kevinhurlbut@sbcglobal.net>
2018-06-29 17:17:17 -0700

    Create validation method

--- src/js/components/Board/Row.js
+++ src/js/components/Board/Row.js
@@ -1,36 +1,41 @@
 import React from "react";
 import Square from "./Square";
 
-let s1 = 0;
+let s0 = 0;
 let pieces = null;
 
 export default class Row extends React.Component {
   constructor(props) {
     super(props);
 
-    if (props.rowNum === undefined) {
-      throw "attribute rowNum is required!";
-    }
-    if (props.rowNum < 0 || props.rowNum > (8 - 1)) {
-      throw "rowNum must be between 0 and 7!"
-    }
+    validate(props);
 
-    s1 = props.rowNum * 8; // Square 1 in Row
+    s0 = props.rowNum * 8; // Square 0 in Row
     pieces = props.pieces;
   }
 
   render() {
     return (
       <div className="board-row">
-        <Square boardIndex={s1 + 0} piece={pieces[s1 + 0]} />
-        <Square boardIndex={s1 + 1} piece={pieces[s1 + 1]} />
-        <Square boardIndex={s1 + 2} piece={pieces[s1 + 2]} />
-        <Square boardIndex={s1 + 3} piece={pieces[s1 + 3]} />
-        <Square boardIndex={s1 + 4} piece={pieces[s1 + 4]} />
-        <Square boardIndex={s1 + 5} piece={pieces[s1 + 5]} />
-        <Square boardIndex={s1 + 6} piece={pieces[s1 + 6]} />
-        <Square boardIndex={s1 + 7} piece={pieces[s1 + 7]} />
+        <Square boardIndex={s0 + 0} piece={pieces[s0 + 0]} />
+        <Square boardIndex={s0 + 1} piece={pieces[s0 + 1]} />
+        <Square boardIndex={s0 + 2} piece={pieces[s0 + 2]} />
+        <Square boardIndex={s0 + 3} piece={pieces[s0 + 3]} />
+        <Square boardIndex={s0 + 4} piece={pieces[s0 + 4]} />
+        <Square boardIndex={s0 + 5} piece={pieces[s0 + 5]} />
+        <Square boardIndex={s0 + 6} piece={pieces[s0 + 6]} />
+        <Square boardIndex={s0 + 7} piece={pieces[s0 + 7]} />
       </div>
     );
   }
+
+  validate(props) {
+    if (props.rowNum === undefined) {
+      throw "attribute rowNum is required!";
+    }
+    if (props.rowNum < 0 || props.rowNum > (8 - 1)) {
+      throw "rowNum must be between 0 and 7!"
+    }
+  }
+
 }
